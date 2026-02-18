076ac38bb78d352b7d26e4cfbb2c985dcdbd33c0
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-02-18 18:26:35 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,42 @@
+variable "location" {
+  description = "Azure region for all resources"
+  type        = string
+  default     = "centralindia"
+}
+
+variable "rg_name" {
+  description = "Resource Group name"
+  type        = string
+  default     = "rg-security-auto"
+}
+
+variable "law_name" {
+  description = "Log Analytics Workspace name"
+  type        = string
+  default     = "law-nsg-sec"
+}
+
+variable "storage_name" {
+  description = "Storage account (must be globally unique, 3-24 lowercase letters/numbers)"
+  type        = string
+  default     = "stnsgflowlogsauto123" # CHANGE if unavailable
+}
+
+variable "nsg_name" {
+  description = "Managed NSG name"
+  type        = string
+  default     = "nsg-app"
+}
+
+variable "logic_app_name" {
+  description = "Logic App name"
+  type        = string
+  default     = "la-nsg-autofix"
+}
+
+variable "protect_ports" {
+  description = "Ports to protect from 0.0.0.0/0"
+  type        = list(string)
+  default     = ["22", "3389"]
+}
 
