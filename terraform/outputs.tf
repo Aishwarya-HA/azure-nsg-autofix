673fc05002a474d2e1fa31c2f56cc5b52e73c2c6
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-02-18 18:27:34 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,13 @@
+output "logic_app_callback_url" {
+  description = "Logic App trigger URL (use this in alerts/webhooks)"
+  value       = azurerm_logic_app_trigger_http_request.la_trigger.callback_url
+  sensitive   = true
+}
 
+output "resource_group" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "nsg_id" {
+  value = azurerm_network_security_group.nsg.id
+}
